770d5b4db45ed80d776a1574a790abeb366b595e
masta <mastanilewicz@gmail.com>
2022-03-24 16:28:12 +0000

    help desk links, fixes on homescreen

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import logo from "../images/Swiftqueue_Logo.png"
 import { useNavigate } from 'react-router-dom'
+import Container from './Container'
 
 const Header = () => {
   const navigate = useNavigate()
 
   return (
-    <div className="header">
-      <img src={logo} alt="Swiftqueue" onClick={() => navigate('/')}/>
-    </div>
+    <Container>
+      <div className="header">
+        <img src={logo} alt="Swiftqueue" onClick={() => navigate('/')}/>
+        <a href="/profile">Go To Your Account</a>
+      </div>
+    </Container>
   )
 }
 export default Header
--- frontend/src/screens/HelpDeskScreen.jsx
+++ frontend/src/screens/HelpDeskScreen.jsx
@@ -12,27 +12,60 @@ const HelpDeskScreen = () => {
       <div className="main">
         <Container>
           <div className="buttons">
-            <button className='span-2'>
+            <button className='btn span-2 blocks-item'>
               <span>BLOOD TESTS</span>
               <span>General FAQ's for how to book.</span>
             </button>
-            <button className='span-2'>
+            <button className='btn span-2 blocks-item'>
               <span>UK COVID19 VACCINATIONS</span>
               <span>General FAQ's for how to book.</span>
             </button>
-            <button className='span-2'>
+            <button className='btn span-2 blocks-item'>
               <span>SCHOOL IMMUNISATIONS</span>
               <span>General FAQ's for how to book.</span>
             </button>
-            <button className='span-3'>
+            <button className='btn span-3 blocks-item'>
               <span>RADIOLOGY (Ultrasounds)</span>
               <span>General FAQ's for how to book.</span>
             </button>
-            <button className='span-3'>
+            <button className='btn span-3 blocks-item'>
               <span>All Other Appoinment Types</span>
               <span>Click here to see FAQ's for any other test types.</span>
             </button>
           </div>
+          <h3>Promoted articles</h3>
+          <ul className='promoted-articles'>
+            <li className='promoted-articles-item'>
+              <a href="/helpdesk">Blood Test Results</a>
+            </li>
+            <li className='promoted-articles-item'>
+              <a href="/helpdesk">How Do I Book My Blood Test Appointment?</a>
+            </li>
+            <li className='promoted-articles-item'>
+              <a href="/helpdesk">How To Cancel My Appointment</a>
+            </li>
+            <li className='promoted-articles-item'>
+              <a href="/helpdesk">I Didn’t Receive An Activation Code As Part Of My Registration</a>
+            </li>
+            <li className='promoted-articles-item'>
+              <a href="/helpdesk">
+How Do I Reschedule My Appointment To Another Date Or Time? How Do I Change This Online?</a>
+            </li>
+            <li className='promoted-articles-item'>
+              <a href="/helpdesk">
+How Do I Reset My Password. I Cannot Login.</a>
+            </li>
+            <li className='promoted-articles-item'>
+              <a href="/helpdesk">
+How to check if I'm eligible for a Covid19 Vaccination Or a Covid Booster Vaccination</a>
+            </li>
+            <li className='promoted-articles-item'>
+              <a href="/helpdesk">Blood Test Results</a>
+            </li>
+            <li className='promoted-articles-item'>
+              <a href="/helpdesk">Blood Test Results</a>
+            </li>
+          </ul>
         </Container>
       </div>
     </div>
