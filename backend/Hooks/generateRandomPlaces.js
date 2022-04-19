d1d148757af790baf7c648b7b11495df91b035f7
masta <mastanilewicz@gmail.com>
2022-04-20 00:17:59 +0100

    generate random time for appointments

--- backend/Hooks/generateRandomPlaces.js
+++ backend/Hooks/generateRandomPlaces.js
@@ -1,15 +1,23 @@
 import { names } from "../data/ClinicNames.js"
 
+const NUMBER_OF_PLACES = 10
+const MAX_APPOINTMENTS_FOR_PLACE = 4
+
 export const generateRandomPlaces = (user, coords) => {
   let generatedPlaces = 0
-  while (generatedPlaces < 5) {
+  while (generatedPlaces < NUMBER_OF_PLACES) {
+    // generate available appointments
+    const appointmets = generateAppointments(Math.floor(Math.random() * MAX_APPOINTMENTS_FOR_PLACE))
+
     // create new place
     const newPlace = {
       name: names[generatedPlaces],
       latitude: (coords[0] + randomNumber(0.5, 20)).toFixed(10),
       longitude: (coords[1] + randomNumber(0.5, 20)).toFixed(10),
+      availableAppointments: [...appointmets]
     }
-    // add new place to previous
+
+    // add new place to previous places
     user.places = [...user.places, { ...newPlace }]
     generatedPlaces += 1
   }
@@ -19,4 +27,19 @@ export const generateRandomPlaces = (user, coords) => {
   }
 
   return user
+}
+
+export const generateAppointments = (n) => {
+  const appointmets = []
+  while (appointmets.length < n + 1) {
+    const add = (Math.floor(Math.random() * 40) * 15)
+    const m = ((480 + add) / 60).toString().split('.')[1]
+    const hour = ((480 + add) / 60).toString().split('.')[0]
+    const min = m === undefined ? '00' : m === '75' ? '45' : m === '25' ? '15' : '00'
+    const newApp = {
+      time: `${hour}:${min}`
+    }
+    appointmets.push(newApp)
+  }
+  return appointmets
 }
