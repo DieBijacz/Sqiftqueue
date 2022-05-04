f2f7c812db44b9ada25ac7d4f17bcd6b5a66c842
masta <mastanilewicz@gmail.com>
2022-05-04 09:49:13 +0100

    booking checkboxes

--- frontend/src/screens/BookingScreen.jsx
+++ frontend/src/screens/BookingScreen.jsx
@@ -4,6 +4,11 @@ import { motion } from 'framer-motion'
 import { pageTransition } from '../animationsVariants'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUserDetails } from '../actions/userActions'
+import hearing from '../images/icons/hearing.png'
+import immobility from '../images/icons/immobility.png'
+import learning_disabilities from '../images/icons/learning_disabilities.png'
+import requires_translator from '../images/icons/requires_translator.png'
+import wheelchair from '../images/icons/wheelchair.png'
 
 const BookingScreen = () => {
   const params = useParams()
@@ -11,6 +16,7 @@ const BookingScreen = () => {
   const navigate = useNavigate()
 
   const [userData, setUserData] = useState(null)
+  const [assistance, setAssistance] = useState([])
 
   const userLogin = useSelector(state => state.userLogin)
   const { userInfo } = userLogin
@@ -28,6 +34,11 @@ const BookingScreen = () => {
     userData && console.log(userData)
   }, [disptach, navigate, user, userInfo, userData])
 
+  function handleCheckbox(e) {
+    // check if there was it before. if thats a case then remove it
+    // else add it to assistance
+  }
+
   return (
     <motion.div className='booking' variants={pageTransition} initial='hidden' animate='show' exit='exit'>
       <div className="blue-strip"></div>
@@ -38,11 +49,63 @@ const BookingScreen = () => {
           </div>
           <div className="body">
             <h4>Appointment For: <span>{userData.name}</span></h4>
+            <hr />
+            <h4>Clinic</h4>
+            <hr />
+            <h4>Appointment Reason</h4>
+            <p>Blood Test · 10 minutes </p>
+            <hr />
+            <h4>Appointment Date/Time</h4>
+            <hr />
+            <h4>Do you require any special assistance?</h4>
+            <div className='assistance'>
+              <div>
+                <div className='checkbox' onClick={(e) => handleCheckbox(e)}>
+                  <input type='checkbox' id='Sight' />
+                  <img src={hearing} alt="Sight" /> Sight
+                </div>
+                <div className='checkbox'>
+                  <input type='checkbox' id='Hearing' />
+                  <img src={hearing} alt="Hearing" /> Hearing
+                </div>
+                <div className='checkbox'>
+                  <input type='checkbox' id='Wheelchair' />
+                  <img src={wheelchair} alt="Wheelchair" /> Wheelchair
+                </div>
+              </div>
+              <div>
+                <div className='checkbox'>
+                  <input type='checkbox' id='Immobility' />
+                  <img src={immobility} alt="Immobility" /> Immobility
+                </div>
+                <div className='checkbox'>
+                  <input type='checkbox' id='Learning' />
+                  <img src={learning_disabilities} alt="Learning Difficulties" /> Learning Difficulties
+                </div>
+                <div className='checkbox'>
+                  <input type='checkbox' id='Translator' />
+                  <img src={requires_translator} alt="Translator" /> Requires Translator/Interpreter
+                </div>
+              </div>
+            </div>
+            <hr />
+            <h4>Additional Comments (optional):</h4>
+            <textarea />
+            <hr />
+            <h4>Terms & Conditions of this Appointment</h4>
+            <div className='checkbox'>
+              <input type='checkbox' id='terms' />
+              <label htmlFor='terms'> I agree to the terms of this appointment</label>
+            </div>
+            <div className='checkbox'>
+              <input type='checkbox' id='gp' />
+              <label htmlFor='gp'> I agree to bring my GP referral card/letter with me to this appointment</label>
+            </div>
           </div>
         </>
         }
-      </main>
-    </motion.div>
+      </main >
+    </motion.div >
   )
 }
 
--- frontend/src/screens/ClinicsScreen.jsx
+++ frontend/src/screens/ClinicsScreen.jsx
@@ -62,69 +62,68 @@ const ClinicsList = () => {
     <motion.div className='clinics-map' variants={pageTransition} initial='hidden' animate='show' exit='exit'>
       <div className='blue-strip'></div>
       <main>
-        <div className='map'>
-          {userData && (
-            <MapContainer center={[51.30, -0.7]} zoom={13} scrollWheelZoom={true}>
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-              />
-              <GoTo location={showLocation ? showLocation : [userData.locations[0].latitude, userData.locations[0].longitude]} />
-              <Marker position={[userData.locations[0].latitude, userData.locations[0].longitude]}>
-                <Popup>
-                  You are here!
-                </Popup>
-              </Marker>
-              {userData.places.map(place => {
-                return <Marker key={place._id} riseOnHover position={[place.latitude, place.longitude]}>
-                  <Popup className='popup'>
-                    <span>{place.name}</span> <br />
-                    {`(${place.phone})`} <br />
-                    <div className="popup-bottom">
-                      <form onSubmit={(e) => bookAppointmentHandler(e)}>
-                        Next available time: <br />
-                        <select onChange={(e) => console.log(e.target.value)}>
-                          <option>Select time</option>
-                          {place.availableAppointments.map(a => <option key={a._id} value={a._id}>{a.time}</option>)}
-                        </select>
-                        <br />
-                        <button type='submit' className='btn'>Book an appointment</button>
-                      </form>
-                    </div>
+        {userData && (
+          <>
+            <div className='map'>
+              <MapContainer center={[51.30, -0.7]} zoom={13} scrollWheelZoom={true}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                />
+                <GoTo location={showLocation ? showLocation : [userData.locations[0].latitude, userData.locations[0].longitude]} />
+                <Marker position={[userData.locations[0].latitude, userData.locations[0].longitude]}>
+                  <Popup>
+                    You are here!
                   </Popup>
                 </Marker>
-              })}
-            </MapContainer>
-          )}
-        </div>
-        {userData && (
-          <motion.div className="locations" variants={container} initial='hidden' animate='show' exit='exit' >
-            {userData.places.map(place => (
-              <motion.div key={place._id} className='location' onClick={(e) => locationClickHandler(place, e)} variants={item} whileHover={{ scale: 1.02 }}>
-                <div className='top'>
-                  <div>
-                    <h1 className='location-place-name'>{place.name}</h1> <br />
-                    {`(${place.phone})`} <br />
-                    8:00 - 18:00
-                  </div>
-                  <div>
-                    <div className='top-right'>
-                      <p>Next available appointment:</p> <br />
-                      {place.availableAppointments.length > 0 && place.availableAppointments[0].time}
+                {userData.places.map(place => {
+                  return <Marker key={place._id} riseOnHover position={[place.latitude, place.longitude]}>
+                    <Popup className='popup'>
+                      <span>{place.name}</span> <br />
+                      {`(${place.phone})`} <br />
+                      <div className="popup-bottom">
+                        <form onSubmit={(e) => bookAppointmentHandler(e)}>
+                          Next available time: <br />
+                          <select onChange={(e) => console.log(e.target.value)}>
+                            <option>Select time</option>
+                            {place.availableAppointments.map(a => <option key={a._id} value={a._id}>{a.time}</option>)}
+                          </select>
+                          <br />
+                          <button type='submit' className='btn'>Book an appointment</button>
+                        </form>
+                      </div>
+                    </Popup>
+                  </Marker>
+                })}
+              </MapContainer>
+            </div>
+            <motion.div className="locations" variants={container} initial='hidden' animate='show' exit='exit' >
+              {userData.places.map(place => (
+                <motion.div key={place._id} className='location' onClick={(e) => locationClickHandler(place, e)} variants={item} whileHover={{ scale: 1.02 }}>
+                  <div className='top'>
+                    <div>
+                      <h1 className='location-place-name'>{place.name}</h1> <br />
+                      {`(${place.phone})`} <br />
+                      8:00 - 18:00
+                    </div>
+                    <div>
+                      <div className='top-right'>
+                        <p>Next available appointment:</p> <br />
+                        {place.availableAppointments.length > 0 && place.availableAppointments[0].time}
+                      </div>
                     </div>
                   </div>
-                </div>
-                {showMoreInfo === place._id && (
-                  <motion.div className="more-info" variants={itemTimes} initial='hidden' animate='show' exit='exit'>
-                    <h1>Times available:</h1>
-                    {place.availableAppointments.map(ap => <div value={ap._id} onClick={() => setBookingTime(ap._id)} className='location-time-available' key={ap._id}>{ap.time}</div>)}
-                  </motion.div>
-                )}
-              </motion.div>
-            ))}
-          </motion.div>
-        )
-        }
+                  {showMoreInfo === place._id && (
+                    <motion.div className="more-info" variants={itemTimes} initial='hidden' animate='show' exit='exit'>
+                      <h1>Times available:</h1>
+                      {place.availableAppointments.map(ap => <div value={ap._id} onClick={() => setBookingTime(ap._id)} className='location-time-available' key={ap._id}>{ap.time}</div>)}
+                    </motion.div>
+                  )}
+                </motion.div>
+              ))}
+            </motion.div>
+          </>
+        )}
       </main >
     </motion.div >
   )
