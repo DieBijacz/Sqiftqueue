0b5a8d9f0cfe007e8d1475bc75727e1a74e6221b
masta <mastanilewicz@gmail.com>
2022-05-12 15:37:25 +0100

    display users appointments in profile page

--- frontend/src/screens/Profile.jsx
+++ frontend/src/screens/Profile.jsx
@@ -26,16 +26,57 @@ const Profile = () => {
     } else {
       dispatch(getUserDetails(userInfo._id)) //get user details
     }
+    console.log(user)
   }, [dispatch, navigate, userInfo])
 
   return (
     <motion.div variants={pageTransition} initial='hidden' animate='show' exit='exit'>
-      <div className="container">
-        <div className="profile">
-          <div>{user && user.email}</div>
-          <button onClick={logoutHandler}>Logout</button>
+      <div className="blue-strip"><h1>Profile</h1></div>
+      {user && (
+        <div className="profile-page">
+          <div className="user-details">
+            <div>
+              <div>{user && user.name}</div>
+              <div>{user && user.email}</div>
+            </div>
+            <button className='btn btn-logout' onClick={logoutHandler}>Log out</button>
+          </div>
+          <div className="summary-panel">
+            <div className="panel">
+              <div className="top">Upcoming Appointments</div>
+              <div className="body">{user.appointmets.length}</div>
+            </div>
+            <div className="panel">
+              <div className="top">Active Referrals</div>
+              <div className="body">0</div>
+            </div>
+            <div className="panel">
+              <div className="top">Past Appointments</div>
+              <div className="body">0</div>
+            </div>
+            <div className="panel">
+              <div className="top">Cancelled Appointments</div>
+              <div className="body">0</div>
+            </div>
+          </div>
+          <div className="appointments">
+            {user.appointmets.map((appointmet, index) => <div key={index} className='appointment'>
+              <div className="info">
+                <div className="appointment-left">
+                  <h1>{appointmet.place.name}</h1>
+                  <div>
+                    Assistances: {appointmet.assistances.map(assistance => <span className='assistance'>{assistance}</span>)}
+                  </div>
+                </div>
+                <div className="appointment-right">
+                  {appointmet.time}
+                </div>
+              </div>
+              <hr />
+            </div>)}
+          </div>
         </div>
-      </div>
+      )}
     </motion.div>
   )
 }
